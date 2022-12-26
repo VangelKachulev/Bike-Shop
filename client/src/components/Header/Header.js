37512babdb3b154e55f21aad904445d8a839166e
VangelKachulev <vangel.kachulev@abv.bg>
2022-12-26 20:52:40 +0200

    update

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -4,7 +4,7 @@ import { useContext } from 'react';
 import { AuthContext } from '../../contexts/AuthContext';
 
 export function Header() {
-    const { token, userData } = useContext(AuthContext);
+    const { token } = useContext(AuthContext);
 
 
     return (
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -5,7 +5,6 @@ export function Home() {
 
         <div className="Mains">
 
-
             <h1 className="Bikeshop">BIKESHOP</h1>
             <h1 className="Trade">the easy way to trade your bike</h1>
             <h3 className="ComStyle">.com</h3>
