c6f7d8d498adbbfa2c1a4d96c8e6c8bf654bbd8e
VangelKachulev <vangel.kachulev@abv.bg>
2023-01-10 22:00:52 +0200

    upd

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,19 +1,18 @@
 
-import styles from './about.module.css';
+import './about.css';
 
 export function About() {
     return (
-        <div className={styles.AboutMainDiv}>
+        <div className="AboutMainDiv">
 
-            <div className={styles.AboutH1}>
-                <h1>About us</h1>
-            </div>
-            <div className={styles.Paragraph}>
-                <p className={styles.BorderParagraph}>We from Bikeshop Ltd. decided to make an easy to use platform for bike lovers.
+            <div className="info-section">
+                <h1>What we do</h1>
+                <p className="BorderParagraph">We from Bikeshop Ltd. decided to make an easy to use platform for bike lovers.
                     Here you can simply trade your old bike and parts. Our goal is to make it
                     as easy as possible for buyers and sellers to get connected.
-                    If you find something wrong don't hesitate to contact us on info@bikeshop.com.
+
                 </p>
+                <p>If you find something wrong don't hesitate to contact us on <strong>info@bikeshop.com.</strong></p>
             </div>
 
         </div>)
